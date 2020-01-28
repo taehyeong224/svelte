729ca5eba8123539c5e9389528a1f4bffbcc81bc
김태형 <taehyeong224@gmail.com>
2020-01-28 23:07:34 +0900

    some test

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,26 @@
 <script>
 	export let name;
+	export let items = [{name: 'hi', qty: 4}, {name: 'hi2', qty: 5} , {name: 'hi3', qty: 6}]
+	export function addItem(target) {
+		items = [...items, {name: target, qty: items[items.length - 1].qty + 1}]
+	}
+	export const addItem2 = target => {items = [...items, {name: target, qty: items[items.length - 1].qty + 1}]}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<input bind:value={name}/>
+	{#if name === '42'}
+		<p>what was the question?</p>
+	{/if}
+
+	<h1>Shopping list</h1>
+	<ul>
+		{#each items as item}
+			<li>{item.name} x {item.qty}</li>
+		{/each}
+	</ul>
+	<button on:click={addItem2(name)}>add</button>
 </main>
 
 <style>
