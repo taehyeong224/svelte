5474485708c3b55c7cdc72f1877e3fc2c93dba0b
김태형 <taehyeong224@gmail.com>
2020-02-01 12:24:29 +0900

    add modal

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,45 @@
 <script>
-	import Header from './header/index.svelte';
+    import Header from './header/index.svelte';
+    import Modal from './modal/index.svelte';
 
-	export let name;
-	export let items = [{name: 'hi', qty: 4}, {name: 'hi2', qty: 5} , {name: 'hi3', qty: 6}]
-	export function addItem(target) {
-		items = [...items, {name: target, qty: items[items.length - 1].qty + 1}]
+    export let items = [{title: '2/2 합시다', body: "블라블라블라블라블라블라블라블라블라블라", isFade: true}, {
+        title: '다음달 하실 분들',
+        body: "블라블라블라블라블라블라블라블라"
+    }, {title: '모이자', body: "블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라"}];
+    export let isFade = true;
+
+    export const modal = {title: "default", body: "default"};
+    export const addItem = ({title, body}) => {
+        items = [...items, {title, body}]
+    };
+    export const showModal = (item) => {
+        isFade = false;
+        modal.title = item.title;
+        modal.body = item.body;
+        console.log(isFade)
+    };
+
+	export const closeModal = () => {
+		isFade = true;
+		console.log(isFade)
+	};
+
+	export const confirmModal = () => {
+		console.log("some logic");
+		closeModal();
 	}
-	export const addItem2 = target => {items = [...items, {name: target, qty: items[items.length - 1].qty + 1}]}
 </script>
 
 <Header/>
 <main>
-	
-	<h1>kth {name}!</h1>
-	<input bind:value={name}/>
-	{#if name === '42'}
-		<p>what was the question?</p>
-	{/if}
-
-	<h1>Shopping list</h1>
-	<ul>
-		{#each items as item}
-			<li>{item.name} x {item.qty}</li>
-		{/each}
-	</ul>
-	<button on:click={addItem2(name)}>add</button>
+    <button>모집하기</button>
+    <h1>현재 진행 중인 해커톤</h1>
+    <ul>
+        {#each items as item}
+            <li on:click={showModal(item)}>{item.title}</li><br/>
+        {/each}
+    </ul>
+	<Modal isFade={isFade} id={"modal"} title={modal.title} body={modal.body} ok="참가하기" closeFunction={closeModal} okFunction={confirmModal}/>
 </main>
 
 <style>
---
+++ src/modal/index.svelte
@@ -0,0 +1,69 @@
+<script>
+    export let isFade;
+    $: console.log(`the isFade is ${isFade}`);
+    export let id;
+    export let title;
+    export let body;
+    export let ok;
+    export let closeFunction;
+    export let okFunction;
+    export const close = () => {
+        closeFunction();
+    };
+    export const confirm = () => {
+        okFunction();
+    }
+</script>
+
+<!-- Modal -->
+{#if !isFade}
+    <modal>
+        <div id={id} class="searchModal">
+            <div class="search-modal-content">
+                <div class="page-header">
+                    <h1>{title}</h1>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <h2>{body}</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal" on:click={close}>닫기
+                    </button>
+                    <button type="button" class="btn btn-primary"
+                            on:click={confirm}>{ok}</button>
+                </div>
+            </div>
+        </div>
+    </modal>
+{/if}
+
+<style>
+    /* The Modal (background) */
+    .searchModal {
+        position: fixed; /* Stay in place */
+        z-index: 10; /* Sit on top */
+        left: 0;
+        top: 0;
+        width: 100%; /* Full width */
+        height: 100%; /* Full height */
+        overflow: auto; /* Enable scroll if needed */
+        background-color: rgb(0, 0, 0); /* Fallback color */
+        background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+    }
+
+    /* Modal Content/Box */
+    .search-modal-content {
+        background-color: #fefefe;
+        margin: 15% auto; /* 15% from the top and centered */
+        padding: 20px;
+        border: 1px solid #888;
+        width: 1000px; /* Could be more or less, depending on screen size */
+    }
+</style>
